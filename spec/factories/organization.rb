e952d0780a13a9ad925206c7acb146ae9c467cd4
Kristine Hines <kristine@3scale.net>
2013-09-30 18:46:12 -0700

    created factory for organizations with categories

--- spec/factories/organization.rb
+++ spec/factories/organization.rb
@@ -2,4 +2,13 @@ FactoryGirl.define do
 	factory :organization do |f|
 		name "WHO"
 	end
+
+	factory :organization_with_categories, :parent => :organization do
+		after(:create) do |organization|
+		  3.times do
+			  create :category, organization: organization
+		  end
+		end	
+	end
+	
 end

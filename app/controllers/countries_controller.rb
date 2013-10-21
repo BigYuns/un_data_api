d3b782083f99c83e62434eb85144af98f3782e72
Kristine Hines <kristine@3scale.net>
2013-10-21 16:09:50 -0700

    removed debuggin statements

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,7 +1,9 @@
 class CountriesController < ApplicationController
+
 	def index
 		organization = Organization.find_by_name(params[:organization])
 		countries = organization.categories.where(name: params[:category]).first.countries.all
 		render json: countries
 	end
+	
 end

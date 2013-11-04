e30396e2f54b6fbb4e51133ec7a20eeb4ceed14c
Kristine Hines <kristine@3scale.net>
2013-11-04 11:07:54 -0800

    switched to using params to obtain app keys

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,18 +7,17 @@ class ApplicationController < ActionController::API
 
 	def index
 		# keep your provider key secret
-		client = ThreeScale::Client.new(:provider_key => ENV['PROVIDER_KEY'] )
+		client = ThreeScale::Client.new(:provider_key => 'fc3033a5413ce5f24239e60150802360')
 
 		# you will usually obtain app_id and app_key from request params
-		response = client.authrep( :app_id => request.headers["app_id"], 
-                           		 :app_key => request.headers["app_key"],
+		response = client.authrep( :app_id => params["app_id"], 
+                           		 :app_key => params["app_key"],
                                :usage => { :hits => 1 })
 
 		if response.success?
   		puts "Application authorized and hit reported!"
 		else
   		puts "Error: #{response.error_message}"
-  		return
 		end
 	end
 

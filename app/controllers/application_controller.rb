4d42b5b21a70960afd5f4086872a60a44db29524
Kristine Hines <kristine@3scale.net>
2013-11-03 22:08:37 -0800

    added code to read request header to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,11 @@ class ApplicationController < ActionController::API
 
 	def index
 		# keep your provider key secret
-		client = ThreeScale::Client.new(:provider_key => ENV['PROVDER_KEY'] )
+		client = ThreeScale::Client.new(:provider_key => ENV['PROVIDER_KEY'] )
 
 		# you will usually obtain app_id and app_key from request params
-		response = client.authrep( :app_id => '#' ,
-                           		 :app_key => '#',
+		response = client.authrep( :app_id => request.headers["app_id"], 
+                           		 :app_key => request.headers["app_key"],
                                :usage => { :hits => 1 })
 
 		if response.success?

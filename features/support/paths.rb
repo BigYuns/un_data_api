2e96521bce42089a3a5133bf69b47997eec561a8
Kristine Hines <kristine@3scale.net>
2013-11-04 16:51:58 -0800

    added keys to the tests, all green again, also created a swagger spec. active docs working on 3scale.net

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -2,16 +2,16 @@ def path_to(page_name)
 	case page_name
 
 	when /\/organizations/
-		get organizations_path(:json)
+		get organizations_path(:json, "app_id" => ENV['APP_ID'], "app_key" => ENV['APP_KEY'])
 
 	when /\/organization\/categories/
-		get categories_path("WHO", :json)
+		get categories_path("WHO", :json, "app_id" => ENV['APP_ID'], "app_key" => ENV['APP_KEY'])
 
 	when /\/organization\/category\/countries/
-		get countries_path("WHO", "Health", :json)
+		get countries_path("WHO", "Health", :json, "app_id" => ENV['APP_ID'], "app_key" => ENV['APP_KEY'])
 
 	when /\/organization\/category\/country\/records/
-		get records_path("WHO", "Health", "USA", :json)
+		get records_path("WHO", "Health", "USA", :json, "app_id" => ENV['APP_ID'], "app_key" => ENV['APP_KEY'])
 
 	end
 end

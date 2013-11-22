c71fc5d26b49685ad230d94d88f9397405260c8c
Kristine Hines <kristine@3scale.net>
2013-11-21 18:48:31 -0800

    more formatting in controllers

--- app/controllers/v1/datasets_controller.rb
+++ app/controllers/v1/datasets_controller.rb
@@ -1,24 +1,24 @@
 module V1
-	class DatasetsController < ApplicationController
+  class DatasetsController < ApplicationController
 
-		def organization_datasets
-			organization = Organization.find_by_name!(params[:organization])
-			datasets = organization.datasets
+    def organization_datasets
+      organization = Organization.find_by_name!(params[:organization])
+      datasets = organization.datasets
 
-			datasets.map! { |dataset| dataset.serializable_hash } 
+      datasets.map! { |dataset| dataset.serializable_hash } 
 
-			respond_with(datasets)
-		end
+      respond_with(datasets)
+    end
 
-		def country_datasets
-			organization = Organization.find_by_name!(params[:organization])
-			country = Country.find_by_name(params[:country])
-			datasets = organization.datasets.where(country_ids: country.id).all
+    def country_datasets
+      organization = Organization.find_by_name!(params[:organization])
+      country = Country.find_by_name(params[:country])
+      datasets = organization.datasets.where(country_ids: country.id).all
 
-			datasets.map! { |dataset| dataset.serializable_hash }
+      datasets.map! { |dataset| dataset.serializable_hash }
 
-			respond_with(datasets)
-		end
+      respond_with(datasets)
+    end
 
-	end
+  end
 end
--- app/controllers/v1/records_controller.rb
+++ app/controllers/v1/records_controller.rb
@@ -1,15 +1,15 @@
 module V1
-	class RecordsController < ApplicationController
+  class RecordsController < ApplicationController
 
-		def index
-			country = Country.find_by_name!(params[:country]) 
-			dataset = Dataset.find_by_name!(params[:dataset]) 
-			records = Record.where(dataset_id: dataset.id, country_id: country.id).all
+    def index
+      country = Country.find_by_name!(params[:country]) 
+      dataset = Dataset.find_by_name!(params[:dataset]) 
+      records = Record.where(dataset_id: dataset.id, country_id: country.id).all
 
-			records.map! { |record| record.serializable_hash }
+      records.map! { |record| record.serializable_hash }
 
-			respond_with(records)
-		end
+      respond_with(records)
+    end
 
-	end
+  end
 end

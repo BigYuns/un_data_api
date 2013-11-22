c587af39ad8493cad72c9247e3424181c12378f8
Kristine Hines <kristine@3scale.net>
2013-11-21 19:30:14 -0800

    reformating spec factories

--- spec/factories/countries.rb
+++ spec/factories/countries.rb
@@ -1,24 +1,24 @@
 FactoryGirl.define do
   factory :country do
-  	name "USA"
+    name "USA"
   end
 
   factory :country_with_organizations, parent: :country do
-  	after(:create) do |country|
-	  	3.times do
-	  		organization = create(:organization)
-	  		country.organizations << organization
-	  	end
-	  end
+    after(:create) do |country|
+      3.times do
+        organization = create(:organization)
+        country.organizations << organization
+      end
+    end
   end
 
   factory :country_with_datasets, parent: :country do
-  	after(:create) do |country|
-  	  3.times do
-  		  dataset = create(:dataset)
-  		  country.datasets << dataset
-  		end
-  	end
+    after(:create) do |country|
+      3.times do
+        dataset = create(:dataset)
+        country.datasets << dataset
+      end
+    end
   end
 
   factory :country_with_records, parent: :country do

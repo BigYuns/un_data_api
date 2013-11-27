9c96fbc348e2596635be357c640b187faa12ac6c
Kristine Hines <kristine@3scale.net>
2013-11-26 16:55:12 -0800

    cleaned up application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,43 +7,43 @@ class ApplicationController < ActionController::API
   before_filter :authenticate_app, :default_format_json
   respond_to :json, :xml
 
-    def create_client
-      @@threescale_client ||= ThreeScale::Client.new(:provider_key => ENV['PROVIDER_KEY'])		
-    end
+  def create_client
+    @@threescale_client ||= ThreeScale::Client.new(:provider_key => ENV['PROVIDER_KEY'])		
+  end
 
-    def response_success(response)
-      if response.success?
-        puts "Application authorized and hit reported!"
-      else
-        error(response.error_code, 401, response.error_message)
-      end
-    end
+  def authenticate_app
+    response = create_client.authrep(:app_id => params["app_id"], 
+                                     :app_key => params["app_key"],
+                                     :usage => { params[:metric].to_sym => 1 })
+    response_success(response)
+  end
 
-    def authenticate_app
-      response = create_client.authrep(:app_id => params["app_id"], 
-                                       :app_key => params["app_key"],
-                                       :usage => { params[:metric].to_sym => 1 })
-      response_success(response)
+  def response_success(response)
+    if response.success?
+      return true
+    else
+      error(response.error_code, 401, response.error_message)
     end
+  end
 
-    def default_format_json
-      request.format = "json" unless params[:format]
-    end
+  def default_format_json
+    request.format = "json" unless params[:format]
+  end
 
-    def invalid_request
-      error(404, 404, "Invalid URL")
-    end
+  def invalid_request
+    error(404, 404, "Invalid URL")
+  end
 
-    private
+  private
 
-    def document_not_found
-      error(404, 404, "Record does not exist")
-    end
+  def document_not_found
+    error(404, 404, "Record does not exist")
+  end
 
-    def error(status, code, message)
-      default_format_json
-      response = { :response_type => "ERROR", :response_code => code, :message => message, :status => status } 
-      respond_with(response)
-    end
+  def error(status, code, message)
+    default_format_json
+    response = { :response_type => "ERROR", :response_code => code, :message => message, :status => status } 
+    respond_with(response)
+  end
 
 end

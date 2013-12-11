1001bdd59b75c4fc667ee5f47c1117558daf9e0b
Kristine Hines <kristine@3scale.net>
2013-12-11 14:12:16 -0800

    removed esd parsing from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -71,20 +71,3 @@ def xml_parser(file, organization_name, dataset_name, measurement)
 		end	
 	end
 end
-
-xml_parser("un_data_xml_files/Precipitation.xml", "Environment Statistics Database", "Precipitation", "million cubic metres")
-xml_parser("un_data_xml_files/actual_evapotranspiration.xml", "Environment Statistics Database", "Actual Evapotranspiration", "million cubic metres")
-xml_parser("un_data_xml_files/freshwater_internal_flow.xml", "Environment Statistics Database", "Freshwater internal flow ", "million cubic metres")
-xml_parser("un_data_xml_files/inflow_of_surface_and_groundwaters.xml", "Environment Statistics Database", "Inflow of surface and groundwaters", "million cubic metres")
-xml_parser("un_data_xml_files/renewable_freshwater_resources.xml", "Environment Statistics Database", "Renewable freshwater resources", "million cubic metres")
-xml_parser("un_data_xml_files/gross_freshwater_abstracted.xml", "Environment Statistics Database", "Gross freshwater abstracted", "million cubic metres")
-xml_parser("un_data_xml_files/gross_fresh_surface_water_abstracted.xml", "Environment Statistics Database", "Gross fresh surface water abstracted", "million cubic metres")
-xml_parser("un_data_xml_files/gross_fresh_groundwater_abstracted.xml", "Environment Statistics Database", "Gross fresh groundwater abstracted", "million cubic metres")
-xml_parser("un_data_xml_files/net_freshwater_supplied_by_water_supply_industry.xml", "Environment Statistics Database", "Net freshwater supplied by water supply industry", "million cubic metres")
-xml_parser("un_data_xml_files/net_freshwater_supplied_by_water_supply_industry_to_households.xml", "Environment Statistics Database", "Net freshwater supplied by water supply industry to: Households", "million cubic metres")
-xml_parser("un_data_xml_files/total_population_supplied_by_water_supply_industry.xml", "Environment Statistics Database", "Total population supplied by water supply industry", "percent")
-xml_parser("un_data_xml_files/population_connected_to_wastewater_collecting_system.xml", "Environment Statistics Database", "Population connected to wastewater collecting system", "percent")
-xml_parser("un_data_xml_files/population_connected_to_wastewater_treatment.xml", "Environment Statistics Database", "Population connected to wastewater treatment", "percent")
-xml_parser("un_data_xml_files/municipal_waste_collected.xml", "Environment Statistics Database", "Municipal waste collected", "1000 tonnes")
-xml_parser("un_data_xml_files/total_population_served_by_municipal_waste_collection.xml", "Environment Statistics Database", "Total population served by municipal waste collection", "percent")
-xml_parser("un_data_xml_files/hazardous_waste_generated.xml", "Environment Statistics Database", "Hazardous waste generated", "tonnes")

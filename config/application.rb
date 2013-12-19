06719cec79142e7b95b84549a153314a6807c584
Kristine Hines <kristine@3scale.net>
2013-12-18 17:07:20 -0800

    change name of env variables

--- config/application.rb
+++ config/application.rb
@@ -26,11 +26,11 @@ module UnDataApi
     end
 
     AWS::S3::Base.establish_connection!(  
-      :access_key_id     => ENV['S3_ACCESS_KEY_ID'],  
-      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']  
+      :access_key_id     => ENV['S3_KEY_ID'],  
+      :secret_access_key => ENV['S3_SECRET_KEY']  
     )  
     
-    BUCKET = ENV['S3_BUCKET_NAME']
+    BUCKET = ENV['S3_BACKUPS_BUCKET']
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers

f84fa50d229057237df203521d53e20f200c45d9
Kristine Hines <kristine@3scale.net>
2013-12-22 15:05:25 -0600

    add tests to footnotes and presence validation on attributes

--- app/models/footnote.rb
+++ app/models/footnote.rb
@@ -2,8 +2,10 @@ class Footnote
   include MongoMapper::Document
 
   key :number, Integer
+  validates_presence_of :number
 
   key :text, String
+  validates_presence_of :text
 
   belongs_to :dataset
 end
---
+++ spec/factories/footnote.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :footnote do |f|
+    text "I am a footnote. lalalala"
+    number 1
+  end
+end

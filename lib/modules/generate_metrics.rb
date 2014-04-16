7efb341b09c85dac4827202a5b51b3c1cb5ac81d
Kristine Hines <hines.kristine@gmail.com>
2014-04-16 12:22:46 -0700

    metrics are working properly

--- lib/modules/generate_metrics.rb
+++ lib/modules/generate_metrics.rb
@@ -1,8 +1,8 @@
 class GenerateMetrics
   include HTTParty
-  base_uri 'undata-api-admin.3scale.net'
+  base_uri @three_scale_url
 
-  def initialize
+  def initialize(three_scale_url)
     @hit_methods_array = ["organizations",
                           "organization_datasets",
                           "countries",
@@ -15,33 +15,34 @@ class GenerateMetrics
                           "db_country_datasets"]
 
     @other_metrics_array = ["invalid_url", "no_record_found"]
+    @three_scale_url = three_scale_url
   end
 
   def get_service_id
     @provider_key_query = { query: { provider_key: ENV['PROVIDER_KEY'] } }
     response = self.class.get('/admin/api/services.xml', @provider_key_query )
     data = response.parsed_response
-    @service_id = data['services']['service'][0]['id']
+    @service_id = data['services']['service']['id']
   end
 
   def get_metric_list
     response = self.class.get('/admin/api/services/' + @service_id + '/metrics.xml', @provider_key_query)
     data = response.parsed_response
-    @hits_metric_id = data['metrics']['metric'][0]['id']
+    @hits_metric_id = data['metrics']['metric']['id']
     create_hits_method
   end
 
   def create_hits_method
+    path = '/admin/api/services/' + @service_id + '/metrics/' + @hits_metric_id + '/methods.xml'
     @hit_methods_array.each do |method|
-      new_method_query = { query: { provider_key: ENV['PROVIDER_KEY'], friendly_name: method, system_name: method, unit: 'hits' } }
-      self.class.post('/admin/api/' + @service_id + '/metrics/' + @hits_metric_id + '/methods.xml', new_method_query)
+      self.class.post(path, body: { provider_key: ENV['PROVIDER_KEY'], friendly_name: method, system_name: method, unit: 'hits' })
     end 
   end
 
   def create_metrics
+    path = '/admin/api/services/' + @service_id + '/metrics.xml'
     @other_metrics_array.each do |metric|
-      new_metric_query = { query: { provider_key: ENV['PROVIDER_KEY'], friendly_name: metric, system_name: metric, unit: 'hits' } }
-      self.class.post('/admin/api/' + @service_id + '/metrics/metrics.xml', new_metric_query)
+      self.class.post(path, body: { provider_key: ENV['PROVIDER_KEY'], friendly_name: metric, system_name: metric, unit: 'hits' })
     end
   end
 
--- lib/tasks/generate_metrics.rake
+++ lib/tasks/generate_metrics.rake
@@ -3,7 +3,7 @@ require "#{Rails.root}/lib/modules/generate_metrics.rb"
 namespace :metrics do
   desc "automatically populate metrics"
   task generate_metrics: :environment do
-    gen_metrics = GenerateMetrics.new
+    gen_metrics = GenerateMetrics.new('your_three_scale_url')
     gen_metrics.get_service_id
     gen_metrics.get_metric_list
     gen_metrics.create_metrics
